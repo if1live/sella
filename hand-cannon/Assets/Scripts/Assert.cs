5d9e26c371c3e64d60df11baaec5293497dc44f7
if1live <libsora25@gmail.com>
2013-11-28 23:09:01 +0900

    assert 추가

---
+++ hand-cannon/Assets/Scripts/Assert.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Assert
+{
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+	public static void Test (bool comparison, string message)
+	{
+		if (!comparison) {
+			Debug.LogWarning (message);
+			Debug.Break ();
+		}
+	}
+
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+	public static void Throw (string message)
+	{
+		Debug.LogWarning (message);
+	}
+
+}
